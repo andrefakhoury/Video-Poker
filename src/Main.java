56dbbc72ee865b80fa64f23faf21a1639932dee4
gsoares1928 <gsilvasoares1928@yahoo.com.br>
2019-03-26 11:29:32 -0300

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
-import java.io.IOException;
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         long creditos = 200;
 
         do {
             System.out.printf("Saldo atual: %d\n", creditos);
             System.out.println("Digite a quantidade que quer apostar.");
-            int aposta = EntradaTeclado.leInt();
+            int aposta = readInt();
+            if (aposta < 0) {
+            	break;
+            }
+            if (aposta > creditos || aposta == 0) {
+            	continue;
+            }
             creditos -= aposta;
 
             Baralho baralho = new Baralho();
@@ -18,13 +24,13 @@ public class Main {
             System.out.println(hand);
 
             System.out.println("Digite as cartas que quer trocar: ");
-            String option = EntradaTeclado.leString();
+            String option = readString();
 
             hand.trocar(option);
             System.out.println(hand);
 
             System.out.println("Digite as cartas que quer trocar: ");
-            option = EntradaTeclado.leString();
+            option = readString();
 
             hand.trocar(option);
             System.out.println(hand);
@@ -32,10 +38,24 @@ public class Main {
             int mult = hand.multiplicadorMao();
             System.out.printf("Conseguiu um profit de %dx...\n", mult);
             creditos += aposta * mult;
-
-        } while (true);
-
-
+        } while (creditos > 0);
 
     }
+    
+    private static int readInt() {
+    	try {
+    		return EntradaTeclado.leInt();
+    	} catch (Exception e) {
+			return 0;
+		}
+    }
+    
+    private static String readString() {
+    	try {
+    		return EntradaTeclado.leString();
+    	} catch (Exception e) {
+    		return "";
+    	}
+    }
+    
 }
